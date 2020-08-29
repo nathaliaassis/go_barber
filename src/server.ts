5ab73ba442b07ca049380de28abcf058b2374551
Nathália <nathaliamassis@gmail.com>
2020-08-29 13:27:41 -0300

    database

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+
+import app from './app';
+
+import './database';
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,35 @@
+import { startOfHour } from 'date-fns';
+import {getCustomRepository} from 'typeorm';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import Appointment from '../models/Appointment';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if(findAppointmentInSameDate){
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+export default CreateAppointmentService;
