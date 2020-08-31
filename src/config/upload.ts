1b7b8fbdd355765b488433e98385ecf914d8e50a
Nathália <nathaliamassis@gmail.com>
2020-08-31 19:27:13 -0300

    avatar update

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,11 +1,13 @@
 import path from 'path';
 import multer from 'multer';
 import crypto from 'crypto';
-import { request } from 'express';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
+  directory: tmpFolder,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const filename = `${fileHash}-${file.originalname}`;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import 'reflect-metadata';
 
-import app from './app';
+import express from 'express';
+import routes from './routes';
 
 import './database';
+import uploadConfig from './config/upload';
 
+const app = express();
+app.use(express.json());
+app.use('files', express.static(uploadConfig.directory));
+app.use(routes);
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
