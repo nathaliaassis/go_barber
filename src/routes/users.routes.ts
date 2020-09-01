e9afe5a630b262d10da8129b9802cce6bbdd7a8d
Nathália <nathaliamassis@gmail.com>
2020-08-31 21:01:24 -0300

    deleting user password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -45,6 +45,8 @@ usersRouter.patch(
         avatarFileName: request.file.filename,
       });
 
+      delete user.password;
+
       return response.json(user);
     } catch (err) {
       return response.status(400).json({ error: err.message });
