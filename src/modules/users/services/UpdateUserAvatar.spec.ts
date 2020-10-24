2fdab4e7f0a2c86e1c5534bd588518def2921cea
Nathália <nathaliamassis@gmail.com>
2020-10-24 14:16:01 -0300

    storage provider

--- src/modules/users/services/UpdateUserAvatar.spec.ts
+++ src/modules/users/services/UpdateUserAvatar.spec.ts
@@ -1,5 +1,44 @@
-describe('UpdateUserAvatar', () => {
-  it('should be able to update user avatar', () => {
-    expect(1 + 2).toBe(3);
-  });
-});
+import { inject, injectable } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class createUserSevice {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider
+  ) { };
+
+  public async execute({ name, email, password }: Request): Promise<User> {
+
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('E-mail address already used.');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default createUserSevice;
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -9,6 +9,7 @@ import AppError from '@shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface Request {
   user_id: string;
@@ -19,7 +20,10 @@ interface Request {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) { };
 
   public async execute({ user_id, avatarFileName }: Request): Promise<User> {
@@ -31,16 +35,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-        console.log('exists');
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFileName;
+    const filename = await this.storageProvider.saveFile(avatarFileName);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
