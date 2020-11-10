94cae74c5f59e2162bcb71a81456bc8d8cebfa32
Nathália <nathaliamassis@gmail.com>
2020-11-10 12:57:30 -0300

    saving user tokens on DB

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,6 +9,9 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 // o registerSingleton instancia a classe uma unica vez em todo ciclo de vida da aplicação
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -18,5 +21,10 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository
 );
 
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,10 +4,13 @@ import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
+import passwordRouter from '@modules/users/infra/http/routes/passwords.routes';
+
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
