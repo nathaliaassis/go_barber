32a67350901902f6850a8c7dff34c0eeb620e716
nathaliaassis <nathaliamassis@gmail.com>
2021-01-09 18:20:22 -0300

    routes validations

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
@@ -16,7 +17,12 @@ appointmentsRouter.use(ensureAuthenticated);
 //   return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post('/', celebrate({
+  [Segments.BODY]: {
+    provider_id: Joi.string().uuid().required(),
+    date: Joi.date(),
+  }
+}), appointmentsController.create);
 appointmentsRouter.post('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
