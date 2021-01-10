233e0fdeb73145d920c736c33bbd8c2c8813ec3a
nathaliaassis <nathaliamassis@gmail.com>
2021-01-09 21:35:18 -0300

    awsa configs

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,11 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/Implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 
@@ -21,5 +24,8 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+
+  mailConfig.driver === 'ethereal' ?
+    container.resolve(EtherealMailProvider) :
+    container.resolve(SESMailProvider)
 );
