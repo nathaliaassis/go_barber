02efbabad4df865e498e67e5e21470fa7c8df56b
nathaliaassis <nathaliamassis@gmail.com>
2021-01-10 00:41:34 -0300

    upload amazon s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,43 @@
 import path from 'path';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk',
+  tmpFolder: string,
+  uploadsFolder: string,
+  multer: {
+    storage: StorageEngine,
+  }
+  config: {
+    disk: {};
+    aws: {
+      bucket: string
+    }
+  };
+}
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const filename = `${fileHash}-${file.originalname}`;
 
-
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const filename = `${fileHash}-${file.originalname}`;
-
-      return callback(null, filename);
-    },
-  }),
-};
+        return callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'gobarber-web'
+    }
+  }
+} as IUploadConfig;
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,15 +1,17 @@
 import { container } from 'tsyringe';
-
+import uploadConfig from '@config/upload';
 import IStorageProvider from '../StorageProvider/models/IStorageProvider';
 
 import DiskStorageProvider from './Implementations/DiskStorageProvider';
+import S3StorageProvider from './Implementations/S3StorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 }
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );
 
