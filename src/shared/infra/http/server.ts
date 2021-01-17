217b9be77b60ff835c1752e0e2d55dbe38c2d204
nathaliaassis <nathaliamassis@gmail.com>
2021-01-16 21:26:06 -0300

    express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -13,9 +13,11 @@ import AppError from '@shared/errors/AppError';
 
 import '@shared/infra/typeorm'
 import '@shared/container';
+import rateLimiter from './middlewares/RateLimiterMIddleware';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
